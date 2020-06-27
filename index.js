00902d3ac62c3d18c3a3bf0bf9d75e6273bfa36e
rednirug <sgurinderpreet@gmail.com>
2020-06-26 23:34:40 -0700

    index.js update

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const util = require("util");
 const fs = require("fs");
 const uuiid = require('uuid/v1');
-
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -15,12 +14,36 @@ class Notes {
   }
 
   retrieval() {
+    return this.read().then(notes => {
+      let parsedNotes;
+      try {
+        parsedNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+        parsedNotes = [];
+      }
+
+      return parsedNotes;
+    });
   }
 
   addNote(note) {
+    const { title, text } = note;
+
+    if (!title || !text) {
+      throw new Error("Note 'title' and 'text' cannot be blank");
+    }
+    const newNote = { title, text, id: uuiid() };
+
+    return this.getNotes()
+      .then(notes => [...notes, newNote])
+      .then(updatedNotes => this.write(updatedNotes))
+      .then(() => newNote);
   }
 
   removal(id) {
+    return this.getNotes()
+      .then(notes => notes.filter(note => note.id !== id))
+      .then(filteredNotes => this.write(filteredNotes));
   }
 }
 
